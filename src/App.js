f2c2a15266083d63d5430f49211ee9b7f8b24839
jonathanert <jomathanerrr@gmail.com>
2024-08-16 03:40:28 +0700

    feat: add soal 2 page

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Route, createBrowserRouter, createRoutesFromElements } from 'react-rout
 import { DefaultLayout } from './layout/DefaultLayout'
 import Home from './pages/Home'
 import Soal1 from './pages/Soal1'
-// import Soal2 from './pages/Soal2'
+import Soal2 from './pages/Soal2'
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<DefaultLayout />}>
       <Route path="/" element={<Home />} />
       <Route path="/soal-1" element={<Soal1 />} />
-      {/* <Route path="/soal-2" element={<Soal2 />} /> */}
+      <Route path="/soal-2" element={<Soal2 />} />
     </Route>,
   ),
 )
---
+++ src/pages/Soal2/index.js
@@ -0,0 +1,9 @@
+import './index.css'
+
+const Soal2 = () => {
+    return (
+        <div>SOAL 2</div>
+    )
+}
+
+export default Soal2
